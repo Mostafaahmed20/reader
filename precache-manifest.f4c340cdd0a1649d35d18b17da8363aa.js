8b8c8c2ded18c9f0028df94588f73e2468e16995
Mostafaahmed20 <mostafa.ahmed00q@gmail.com>
2020-06-06 15:02:16 +0200

    Updates

---
+++ precache-manifest.f4c340cdd0a1649d35d18b17da8363aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebe5d79e4365ffa133cb138e1631b26",
+    "url": "/reader/index.html"
+  },
+  {
+    "revision": "d008f4c6a0a48b996d0d",
+    "url": "/reader/static/css/main.3eececea.chunk.css"
+  },
+  {
+    "revision": "3b5d78b1bcfe76b6d8c8",
+    "url": "/reader/static/js/2.8c440bc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reader/static/js/2.8c440bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d008f4c6a0a48b996d0d",
+    "url": "/reader/static/js/main.54292dd8.chunk.js"
+  },
+  {
+    "revision": "062a364b7f0d21c46919",
+    "url": "/reader/static/js/runtime-main.ba344254.js"
+  }
+]);
